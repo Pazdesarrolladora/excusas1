b0893c8a74e47470e76022715eaa7a81bae6adf8
Paz Valenzuela <pazvalenzu@gmail.com>
2023-05-10 17:56:09 -0700

    2.0

--- excusas/excusas.js
+++ excusas/excusas.js
@@ -1,12 +1,12 @@
-let who = ['the dog','my granma','his turtle','my bird'];
-let what = ['eat','pissed','crushed','broked'];
-let when = ['before the class','right in time','when I finished','during my lunch','while I was praying'];
+let who = ['the dog', 'my granma', 'his turtle', 'my bird', 'the aliens', 'the government', 'the Illuminati'];
+let what = ['eat', 'pissed', 'crushed', 'broked', 'burned', 'exploded', 'deleted'];
+let when = ['before the class', 'right in time', 'when I finished', 'during my lunch', 'while I was praying', 'in a parallel universe', 'in a previous life'];
 
  function generateExcuse() {
   let randomWho = who[Math.floor(Math.random() * who.length)];
   let randomWhat = what[Math.floor(Math.random() * what.length)];
   let randomWhen = when[Math.floor(Math.random() * when.length)];
 
-  let excuse = randomWho + " " + randomWhat + " my homework " + randomWhen + ".";
+  let excuse = randomWho + " " + randomWhat + " my hearth " + randomWhen + ".";
   document.getElementById("excuse").innerHTML = excuse;
 }
